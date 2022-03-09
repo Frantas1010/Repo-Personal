bc033b8d7857817a537f02d209177afed49a690c
Franco Machado <fmachado@escuelasproa.edu.ar>
2022-03-09 10:13:50 -0300

    VAMOS LO CONSEGUI

--- practica Express/index.js
+++ practica Express/index.js
@@ -2,15 +2,17 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const path = require("path");
-const bodyparser = require("Body-parser");
+const bodyparser = require("body-parser");
 const ruta = path.join(__filename, '../index.html');
+app.use(bodyparser.urlencoded({extended:false}));
+app.use(bodyparser.json());
 
 app.get("/page", (req, res) => {
     res.sendFile(ruta);
 });
 
 app.post("/page/tunombre", (req, res) => {
-    res.send(req);
+    res.send("hola " + req.body.user_name + " " + req.body.user_name2);
 });
 
 app.listen(port, () => {
